86b74c56076178d189602fc7bf3aaf4026dc0efc
Michael Shilman <shilman@users.noreply.github.com>
2024-12-15 20:27:56 +0800

    Fix Git commit icon

--- packages/react/src/PageHeader/PageHeader.examples.stories.tsx
+++ packages/react/src/PageHeader/PageHeader.examples.stories.tsx
@@ -20,7 +20,7 @@ import {
   GitBranchIcon,
   CodeIcon,
   CommentDiscussionIcon,
-  CommitIcon,
+  GitCommitIcon,
   ChecklistIcon,
   FileDiffIcon,
   TriangleDownIcon,
@@ -43,6 +43,7 @@ const meta: Meta = {
     controls: {expanded: true},
   },
   args: {},
+  tags: ['!test'],
 }
 
 const setViewportParamToNarrow = {
@@ -126,7 +127,7 @@ export const PullRequestPage = () => (
           <UnderlineNav.Item icon={CommentDiscussionIcon} counter="12" aria-current="page">
             Conversation
           </UnderlineNav.Item>
-          <UnderlineNav.Item counter={3} icon={CommitIcon}>
+          <UnderlineNav.Item counter={3} icon={GitCommitIcon}>
             Commits
           </UnderlineNav.Item>
           <UnderlineNav.Item counter={7} icon={ChecklistIcon}>
@@ -352,7 +353,7 @@ export const WithPageLayout = () => {
               <UnderlineNav.Item icon={CommentDiscussionIcon} counter="12" aria-current="page">
                 Conversation
               </UnderlineNav.Item>
-              <UnderlineNav.Item counter={3} icon={CommitIcon}>
+              <UnderlineNav.Item counter={3} icon={GitCommitIcon}>
                 Commits
               </UnderlineNav.Item>
               <UnderlineNav.Item counter={7} icon={ChecklistIcon}>
